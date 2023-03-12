99de60a6ec07f6b02a0120cd2a54d32629bbd4f8
L E <erickson.leah.j@gmail.com>
2023-03-12 15:05:15 -0600

    added if statement and alert for length

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -12,13 +12,15 @@ var symbols = ["!","@","#","$","%","^","&","*"];
 function generatePassword(length, hasLowercase, hasUppercase, hasNumbers, hasSymbols) {
   
   var length = prompt("How many characters would you like to include?");
+  if(length < 8 || length > 128);
+  alert("Must include 8-128 characters");
   
   var lowerLetters = prompt("would you like to include lowercase letters?");
   var upperLetters = prompt("Would you like to include uppercase letters?");
   var numbers = prompt("Would you like to include numbers?");
   var symbols = prompt("Would you like to include symbols?");
   var availableCharacters = [];
-  
+
   if(hasLowercase) {
     availableCharacters = [lowerLetters];
   }
