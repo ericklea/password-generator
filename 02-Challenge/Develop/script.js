ddd07927cbc47a23bc7d48a651e9d0bfbd26ea46
L E <erickson.leah.j@gmail.com>
2023-03-12 14:39:35 -0600

    added lowercase letter prompt

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -8,9 +8,10 @@ var symbols = ["!","@","#","$","%","^","&","*"];
 
 
 
+
 function generatePassword(length, hasLowercase, hasUppercase, hasNumbers, hasSymbols) {
   var availableCharacters = [];
-
+  var lowerLetters = prompt("would you like to include lowercase letters?")
   
   if(hasLowercase) {
     availableCharacters = [lowerLetters];
