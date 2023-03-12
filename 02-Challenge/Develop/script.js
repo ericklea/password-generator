d10ccec989ec2aa0a15fc1b716f490b0fe97b205
L E <erickson.leah.j@gmail.com>
2023-03-12 10:59:22 -0600

    rearranged variables

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [0,1,2,3,4,5,6,7,8,9];
-var symbols = ["!","@","#","$","%","^","&","*"];
 var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var symbols = ["!","@","#","$","%","^","&","*"];
 
 
 function generatePassword(length, hasLowercase, hasUppercase, hasNumbers, hasSymbols) {
