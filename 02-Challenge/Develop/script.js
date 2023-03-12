1b169f1a2697a55f1a6ad6e9cc9ae2c7ed3aca11
L E <erickson.leah.j@gmail.com>
2023-03-12 10:16:14 -0600

    added available character variable and adjusted booleans

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -7,8 +7,8 @@ var lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
 var upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 
-function generatePassword = (length, hasLowercase, hasUppercase, hasNumbers, hasSymbols) {
-  
+function generatePassword = (length, _hasLowercase, _hasUppercase, _hasNumbers, _hasSymbols) {
+  var availableCharacters =
 
 }
 
