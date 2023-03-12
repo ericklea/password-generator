5d461a8908a84733f83837df02f334be366a6d05
L E <erickson.leah.j@gmail.com>
2023-03-12 09:47:24 -0600

    addednumber array

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+
+
 function generatePassword() {
 
   var plength = prompt("How many characters would you like to include?")
